2912c477715e627b3fb59b3b5233ee6942e94399
Maria <marianenacarvalho@gmail.com>
2023-10-23 20:48:03 -0300

    Adicionando funcionalidades da API ao schema de items

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -16,6 +16,63 @@ const itemController = {
         } catch (error) {
             console.log(error)
         }
+    },
+    getAll: async (req, res) => {
+        try {
+            const items = await ItemModel.find();
+            res.json(items);
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    get: async (req, res) => {
+        try {
+            // id => URL === GET, passar id pela URL
+            const id = req.params.id
+            const item = await ItemModel.findById(id);
+
+            if (!item){
+                res.status(404).json({msg: "Item não encontrado."});
+                return;
+            }
+
+            res.json(item);
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const item = await ItemModel.findById(id);
+            if(!item){
+                res.status(404).json({msg: "Item não encontrado."})
+                return;
+            }
+
+            const deletedItem = await ItemModel.findByIdAndDelete(id);
+            res.status(200).json({ deletedItem, msg: "Item deletado com sucesso."})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    update: async (req, res) => {
+
+        try {
+            const id = req.params.id;
+
+            const item = {
+                nome: req.body.nome,
+                descricao: req.body.descricao,
+                preco: req.body.preco,
+                categoria: req.body.categoria,
+            }
+
+            const updatedItem = await ItemModel.findByIdAndUpdate(id, item);
+            res.status(200).json({updatedItem, msg: "Item atualizado com sucesso."})
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
