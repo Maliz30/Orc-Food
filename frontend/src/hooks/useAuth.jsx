9fac86286b512e2821c7530aba047aed5d26e900
Maria <marianenacarvalho@gmail.com>
2023-11-05 23:37:40 -0300

    Criando base de autenticação

---
+++ frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,15 @@
+// api
+import api from '../utils/api'
+/* 
+import {useState, useEffect} from 'react'
+import {useHistory} from 'react-router-dom' */
+
+export default function useAuth(){
+    async function register(user){
+        try {
+            const data = await api.post()
+        } catch (error) {
+            
+        }
+    }
+}
