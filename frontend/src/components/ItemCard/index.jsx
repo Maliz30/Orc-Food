b813ccc7b0abd8b0c3e7fe283ec46752d06cf1d0
Maria <marianenacarvalho@gmail.com>
2023-11-06 00:52:43 -0300

    Adicionando elementos do ADM no card de items

--- frontend/src/components/ItemCard/index.jsx
+++ frontend/src/components/ItemCard/index.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
+import { Context } from '../../context/UserContext'
+import Delete from '../../assets/img/delete.svg'
+import Edit from '../../assets/img/edit.svg'
 
 const ItemCard = props => {
+    const { authenticated } = useContext(Context)
+
     return (
         <div className='card-wrapper'>
-            <div className='nomedescricao-section'>
-                <span className='item-nome'>
-                    {props.item.nome}
-                </span>
-                <div className='text-section'>
-                    <span>{props.item.categoria}</span>
+            <div className='item-card-header'>
+                <div className='nomedescricao-section'>
+                    <span className='item-nome'>
+                        {props.item.nome}
+                    </span>
+                    <div className='text-section'>
+                        <span>{props.item.categoria}</span>
+                    </div>
                 </div>
+                {authenticated ? <div className='buttons-wrapper'>
+                    <button className='adm-button'><img src={Delete} alt="" /></button>
+                    <button className='adm-button'><img src={Edit} alt="" /></button>
+                </div> : <p></p>}
             </div>
+
             <div className='text-section desc-item'>
                 {props.item.descricao}
             </div>
